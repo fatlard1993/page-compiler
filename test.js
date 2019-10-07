8a50396dcc42ddb33c2624d1930da2961251c620
fatlard1993 <fatlard1993@gmail.com>
2019-10-07 13:08:24 -0700

    add tests

---
+++ test.js
@@ -0,0 +1,29 @@
+const run = require('ava');
+
+const pageCompiler = require('./src/index');
+
+pageCompiler.prebuilt.prebuilt = '<div></div>';
+
+run('build prebuilt', (test) => {
+	var page = pageCompiler.build('prebuilt');
+
+	test.is(page.length, 104);
+});
+
+run('build error', (test) => {
+	var page = pageCompiler.build('error', 'test');
+
+	test.is(page.length, 199);
+});
+
+run('build one', (test) => {
+	var page = pageCompiler.build('one');
+
+	test.is(page.length, 234);
+});
+
+run('build two', (test) => {
+	var page = pageCompiler.build('two');
+
+	test.is(page.length, 199);
+});
